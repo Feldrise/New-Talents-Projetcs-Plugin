5b2295e5576511947fe590db781db08150da2cde
Feldrise <admin@feldrise.com>
2020-04-26 22:03:04 +0200

    [Update] full message for Discord

--- class.project.php
+++ class.project.php
@@ -1,18 +1,45 @@
 <?php
 // This class represent a project.
 class NtfProject {
+    public $id = ''; // This id correspond to the post id
+
     public $title = '';
+    public $author = '';
     public $category = '';
     public $webhook = '';
 
+    public $launchDate = '';
     public $description = '';
+    public $novelty = '';
+    public $team = '';
+    public $isLucratif = false;
+    public $isSearchingPeople = false;
 
-    public function __construct($title, $category, $webhook, $description) {
-        $this->title = $title;
-        $this->category = $category;
-        $this->webhook = $webhook;
+    public $errorMessage = '';
 
-        $this->description = $description;
+    // public function __construct($title, $category, $webhook, $description) {
+    //     $this->title = $title;
+    //     $this->category = $category;
+    //     $this->webhook = $webhook;
+
+    //     $this->description = $description;
+    // }
+
+    // This function allows to create the project from the post data of the form in
+    // the widget
+    public function construct_from_form($form_data, $category, $webhook) {
+        $this->title = $form_data['project_name'];
+        $this->author = $form_data['project_author'];
+        $this->category = $category; // Can't be deduced from form post
+        $this->webhook = $webhook; // Can't be deduced from form post
+
+        $this->launchDate = date('Y-m-d', strtotime($form_data['project_launch_date']));
+        $this->description = $form_data['project_description'];
+        $this->novelty = $form_data['project_novelty'];
+        $this->team = $form_data['project_team'];
+        
+        $this->isLucratif = isset($form_data['project_is_lucratif']);
+        $this->isSearchingPeople = isset($form_data['project_is_searching_people']);
     }
 
     // This function create an array which represent a post for Wordpress
@@ -31,13 +58,40 @@ class NtfProject {
     public function to_discord_post() {
         return json_encode([
             // The general "message" shown above your embeds
-            "content" => $this->description,
+            "content" => "",
             // The username shown in the message
             "username" => "Projet",
             // The image location for the senders image
             "avatar_url" => "https://new-talents.fr/wp-content/uploads/2019/07/mini.png",
             // Whether or not to read the message in Text-to-speech
             "tts" => false,
+            // The embeds
+            "embeds" => [
+                // The first embeds with most informations
+                [
+                    "title" => $this->title,
+                    // The type of your embed, will ALWAYS be "rich"
+                    "type" => "rich",
+
+                    "description" => "**Description du projet :** $this->description\n\n**Originalité du projet :** $this->novelty\n\n**L'équipe du projet :** $this->team",
+
+                    "url" => get_permalink($this->id),
+                    
+                    "color" => hexdec( "d81b60" ),
+
+                    "author" => [
+                        "name" => $this->author
+                    ],
+
+                    "footer" => [
+                        "text" => "Lancement le " . date('d/m/Y', $this->launchDate)
+                    ]
+                ],
+                [
+                    "description" => (($this->isLucratif) ? 'Ce projet est lucratif' : "Ce projet n'est pas lucratif") . " et " . (($this->isSearchingPeople) ? 'recherche de nouveaux profils' : 'ne recherche pas de nouveaux profils'),
+                    "color" => hexdec( "f5ff00" )
+                ]
+            ]
         ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
     }
 }
--- class.widgets.project-form.php
+++ class.widgets.project-form.php
@@ -3,6 +3,8 @@
 class WidgetProjectForm {
     public static $channels = array(); // We store channels in an array
 
+    public $statusMessage = ''; // We can show the current statue of the form
+
     // Init the plugin
     public static function init() {
         // We add all channels 
@@ -14,13 +16,13 @@ class WidgetProjectForm {
     public static function project_submit() {
         if(isset($_POST['project-send'])) {
             // We create the post object from the info
-            $project = new NtfProject(
-                $_POST['project_name'],                                        // The project name
+            $project = new NtfProject();
+            $project->construct_from_form(
+                $_POST, 
                 self::$channels[$_POST['project_category']]->category_id,      // The project category id on New Talents site
-                self::$channels[$_POST['project_category']]->webhook,          // The project category webhook on Discord
-                $_POST['project_description']                                  // The project description
+                self::$channels[$_POST['project_category']]->webhook
             );
-
+            
             // We add the post on the site
             self::submit_wordpress_post($project);
 
@@ -34,18 +36,27 @@ class WidgetProjectForm {
 ?>
 <form class="project-submition-form" method="post" action="#">
     <p class="project-submition-form-select">
+        <label>Catégorie du projet : </label>
         <select id="project_category" name="project_category">
 <?php
         // We show each channels on the select
         foreach (self::$channels as $key => $channel) {
-            echo('<option value=' . $key . '>' . $channel->name . '</option>');
+            echo("<option value=\"$key\">$channel->name</option>");
         }
 ?>
         </select>
     </p>
     <p>
-        <label>Nom du projet : </label><input type="text" id="project_name" name="project_name" placeholder="Nom du projet" />
-        <label>Description du projet : </label><textarea id="project_description" name="project_description" placeholder="Description du projet" row="4"></textarea>
+        <label>Nom du projet : </label><input type="text" id="project_name" name="project_name" placeholder="Nom du projet" required/>
+        <label>Nom de l'auteur : </label><input type="text" id="project_author" name="project_author" placeholder="Nom de l'auteur" required/>
+        <label>Date de lancement : </label><input type="date" id="project_launch_date" name="project_launch_date" value="<?php echo date('Y-m-d'); ?>" required/><br/>
+        <label>Description du projet : </label><textarea id="project_description" name="project_description" placeholder="Description du projet" row="4" required></textarea>
+        <label>Qu'est-ce que votre projet apporte de nouveau ? : </label><textarea id="project_novelty" name="project_novelty" placeholder="Qu'est-ce que votre projet apporte de nouveau ?" row="4" required></textarea>
+        <label>Constitution de l'équipe : </label><input type="text" id="project_team" name="project_team" placeholder="Constitution de l'équipe" required/>
+    </p>
+    <p>
+        <label for="project_is_lucratif">Le projet est à but lucratif </label><input type="checkbox" id="project_is_lucratif" name="project_is_lucratif" /><br/>
+        <label for="project_is_searching_people">Le projet cherche de nouveaux profils </label><input type="checkbox" id="project_is_searching_people" name="project_is_searching_people" />
     </p>
     <p class="project-submition-form-submit">
         <input type="submit" name="project-send" value="Envoyer" />
@@ -62,6 +73,8 @@ class WidgetProjectForm {
             // throw new Exception($new_post_id->get_error_message(), 1);
             echo ($new_post_id->get_error_message());
         }
+        
+        $project->id = $new_post_id;
     }
 
     // We show the project in the corresponding channel on the Discord server
